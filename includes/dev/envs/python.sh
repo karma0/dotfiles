985540e08d0e2f6c9aba828b5151d35c2b7833b4
Bobby <karma0@gmail.com>
2017-09-16 04:46:15 +0000

    Removed virtualenvwrapper in favor of pyenv

--- includes/dev/envs/python.sh
+++ includes/dev/envs/python.sh
@@ -1,9 +1,14 @@
-plugins+=( python pep8 pylint virtualenv virtualenvwrapper )
+plugins+=( \
+  python \
+  pep8 \
+  pylint \
+  #virtualenv \
+  #virtualenvwrapper \
+)
 
 # Python virtualenvwrapper
 export WORKON_HOME="$HOME/src/python"
-source /usr/bin/virtualenvwrapper.sh
 
 # Personal python library
-export PYTHONPATH="$PYTHONPATH:$HOME/src/python/wild_things/wild_things"
-
+[ -d "$HOME/src/python/wild_things/wild_things" ] && \
+  export PYTHONPATH="$PYTHONPATH:$HOME/src/python/wild_things/wild_things"
