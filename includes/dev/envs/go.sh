f55fae6aaa1699779b728a2738afb1dd52b336df
Bobby <karma0@gmail.com>
2017-09-16 04:52:01 +0000

    Added cdpath to go env

--- includes/dev/envs/go.sh
+++ includes/dev/envs/go.sh
@@ -1,3 +1,10 @@
-export GOPATH="$HOME/go:$HOME/src/go"
-export PATH="$PATH:$HOME/src/go/bin:$HOME/go/bin"
+export GOPATH="$HOME/go"
+export PATH="$PATH:$HOME/go/bin"
+
 plugins+=( go golang )
+cdpath+=( \
+  "$HOME/go/src/github.com/karma0" \
+  "$HOME/go/src/github.com/RobotStudio" \
+  "$HOME/go/src/github.com/LarsonAgency" \
+  "$HOME/go/src"
+)
